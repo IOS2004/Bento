395b28749bad26d3eb93bb807a61cebe09fee8bf
Om Sahu <97842790+IOS2004@users.noreply.github.com>
2023-12-17 10:22:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: 'b96462901c7ae7bffb2104b869ac6f51', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
